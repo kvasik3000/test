71278d988945f1e30b83b770a69dec7b4077368c
kvasik3000 <114009968+kvasik3000@users.noreply.github.com>
2023-03-19 22:18:40 +0700

    исправление ошибок

--- task2/task.cpp
+++ task2/task.cpp
@@ -6,7 +6,7 @@
 #include <ctype.h>
 #include <string>
 #include <cstring>
-
+#include <algorithm>
 
 int main(int argc, char* argv[]){
     auto start = std::chrono::high_resolution_clock::now();
@@ -15,69 +15,74 @@ int main(int argc, char* argv[]){
     int iter = 0;
     int iter_max = std::stoi(argv[6]);
     int tol = std::stoi(argv[4]);
-    // double
+
     double **matrix = new double*[tol];
-    // double **new_matrix = new double*[tol];
+    double **new_matrix = new double*[tol];
 
     for (int i = 0;i < tol;  ++i)
     {
         matrix[i] = new double[tol];
-        // new_matrix[i] = new double[tol];
+        new_matrix[i] = new double[tol];
         for(int j = 0; j < tol; ++j){
-            matrix[i][j] = 20.0;
-            // new_matrix[i][j] = 20;
+            matrix[i][j] = 20;
+            new_matrix[i][j] = 20;
         }
     }
+    #pragma acc enter data create(matrix[0:tol][0:tol],new_matrix[0:tol][0:tol],iter) copyin(tol,err)
 
-    #pragma acc enter data create(matrix[0:tol][0:tol],iter) copyin(tol,err)
+    matrix[0][0] = 10;
+    matrix[0][tol-1] = 20;
+    matrix[tol-1][0] = 20;
+    matrix[tol-1][tol-1] = 30;
 
-    matrix[0][0] = 10.0;
-    matrix[0][tol-1] = 20.0;
-    matrix[tol-1][0] = 20.0;
-    matrix[tol-1][tol-1] = 30.0;
+    new_matrix[0][0] = 10;
+    new_matrix[0][tol-1] = 20;
+    new_matrix[tol-1][0] = 20;
+    new_matrix[tol-1][tol-1] = 30;
 
-    // new_matrix[0][0] = 10;
-    // new_matrix[0][tol-1] = 20;
-    // new_matrix[tol-1][0] = 20;
-    // new_matrix[tol-1][tol-1] = 30;
-
-    #pragma acc update device(matrix[:tol][:tol])
+    #pragma acc update device(matrix[:tol][:tol],new_matrix[:tol][:tol])
     
 
-    #pragma acc parallel loop present(matrix[:tol][:tol]) 
-    
-    // #pragma acc enter data copyin(step)
+    #pragma acc parallel loop present(matrix[:tol][:tol],new_matrix[:tol][:tol  ]) 
+
     for (int i = 1; i < tol-1; ++i)
     {
-
         double step = (10.0 /(tol-1))*i;
         matrix[0][i]=matrix[0][0] + step;
         matrix[i][0]=matrix[0][0] + step;
         matrix[tol-1][i]=matrix[tol-1][0] + step;
         matrix[i][tol-1]=matrix[0][tol-1] + step;
-        // new_matrix[0][i]=new_matrix[0][0] + step;
-        // new_matrix[i][0]=new_matrix[0][0] + step;
-        // new_matrix[tol-1][i]=new_matrix[tol-1][0] + step;
-        // new_matrix[i][tol-1]=new_matrix[0][tol-1] + step;
+        new_matrix[0][i]=new_matrix[0][0] + step;
+        new_matrix[i][0]=new_matrix[0][0] + step;
+        new_matrix[tol-1][i]=new_matrix[tol-1][0] + step;
+        new_matrix[i][tol-1]=new_matrix[0][tol-1] + step;
+        
     }
     
-    
+    // int it = 0;
     while(err > error && iter<iter_max){
         err = 0.0;
         #pragma acc update device(err)
 
-        #pragma acc parallel loop collapse(2) present(matrix[0:tol][0:tol],tol,err) independent reduction(max:err)
+        #pragma acc parallel loop collapse(2) present(matrix[0:tol][0:tol],new_matrix[0:tol][0:tol],tol,err) independent reduction(max:err)
         for (int i = 1; i < tol - 1; ++i)
         {
             for (int j = 1; j < tol-1; ++j)
             {
-                double step_m = matrix[i][j];
-                matrix[i][j] = 0.25 *( matrix[i][j+1] + matrix[i][j-1]+ matrix[i-1][j] + matrix[i+1][j]);
-                err = std::max(err,step_m - matrix[i][j]);
+                new_matrix[i][j] = 0.25 *( matrix[i][j+1] + matrix[i][j-1]+ matrix[i-1][j] + matrix[i+1][j]);
+                err = fmax(err,fabs(new_matrix[i][j] - matrix[i][j]));
             }
             
         }
-
+        // std::copy_n(new_matrix[it], tol, matrix[it]);
+        // swap(matrix,new_matrix);
+        double **step = matrix;
+        matrix = new_matrix;
+        new_matrix = step;
+       
+        // it++;
+        
+        
         iter++;
         #pragma acc update self(err)
     }
@@ -97,13 +102,15 @@ int main(int argc, char* argv[]){
         
     // }
     
-    #pragma acc exit data delete(matrix[:tol][:tol], error)
+    #pragma acc exit data delete(matrix[:tol][:tol], new_matrix[:tol][:tol], error)
 
     for (int i = 0; i < tol; ++i){
         delete[] matrix[i];
-        
+        delete[] new_matrix[i];
     }
     delete[] matrix;
+    delete new_matrix;
+
     
     auto elapsed = std::chrono::high_resolution_clock::now() - start;
 	long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
