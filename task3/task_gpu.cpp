b41c50430de0401602e6654a2e1c7e00db2cc47f
kvasik3000 <114009968+kvasik3000@users.noreply.github.com>
2023-05-03 05:47:01 +0700

    Update task_gpu.cpp
    вычисление  максимальной ошибки с использованием библиотеки cublas, подсчет ошибки каждую итерацию

--- task3/task_gpu.cpp
+++ task3/task_gpu.cpp
@@ -7,16 +7,23 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
 
-void init_array(double **matrix, int tol)
+// определим макрос с помощью которого проведем индексацию, так как библиотека cublas совместима с Fortran использует хранение по столбцам и индексацию на основе 1
+// и по этому не может использовать родную индексицию массивов
+// i - line, j - column, ld - matrix size 
+#define IDX2C(i,j,ld) (((j)*(ld))+(i)) 
+
+void init_array(double *matrix, int tol)
 {
     #pragma acc parallel present(matrix)
     {
         //происходит заполнение углов матрицы
-        matrix[0][0] = 10;
-        matrix[0][tol-1] = 20;
-        matrix[tol-1][0] = 20;
-        matrix[tol-1][tol-1] = 30;
+        matrix[IDX2C(0, 0, tol)] = 10.0;
+        matrix[IDX2C(0, tol-1, tol)] = 20.0;
+        matrix[IDX2C(tol-1,0, tol)] = 20.0;
+        matrix[IDX2C(tol-1, tol-1, tol)]= 30.0;
 
         //заполняем матрицу по периметру
         double step = (10.0 /(tol-1));
@@ -25,10 +32,10 @@ void init_array(double **matrix, int tol)
         for (int i = 1; i < tol-1; ++i)
         {
             double step_i = step * i;
-            matrix[0][i]=matrix[0][0] + step_i;
-            matrix[i][0]=matrix[0][0] + step_i;
-            matrix[tol-1][i]=matrix[tol-1][0] + step_i;
-            matrix[i][tol-1]=matrix[0][tol-1] + step_i;
+            matrix[IDX2C(0, i, tol)]=matrix[IDX2C(0, 0, tol)] + step_i;
+            matrix[IDX2C(i, 0, tol)]=matrix[IDX2C(0, 0, tol)] + step_i;
+            matrix[IDX2C(tol-1,i, tol)]=matrix[IDX2C(tol-1,0, tol)] + step_i;
+            matrix[IDX2C(i, tol-1, tol)]=matrix[IDX2C(0, tol-1, tol)] + step_i;
         }
         //Массив инициализирую значениями 20 , так как 20 это среднее значение по углам сетки, это значит,
         //что до основного кода заполнения матрицы, ответы будут уже приближены к правильным,
@@ -49,26 +56,38 @@ void init_array(double **matrix, int tol)
 
 
 int main(int argc, char* argv[]){
+    
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    cublasStatus_t status;
+
     auto start = std::chrono::high_resolution_clock::now();
     double err = std::stod(argv[2]);
-    double error = 0.0;
+    double error = 1.0;
     int iter = 0;
     int iter_max = std::stoi(argv[6]);
     int tol = std::stoi(argv[4]);
-    //создаем две дмумерные матрицы
+    int size = tol*tol;
+    // скаляр для вычитания
+    double alpha = -1;
+    int step_inc = 1;//шаг инкремента
+    int max_ind = 0;
 
-    double **matrix = new double*[tol];
-    double **new_matrix = new double*[tol];
+    //создаем два вектора и доп вектор
 
-    for (int i = 0;i < tol;  ++i)
-    {   
-        matrix[i] = new double[tol];
-        new_matrix[i] = new double[tol];        
-    }
+    double *matrix = new double [size];
+    double *new_matrix = new double [size];
+    double *step_matrix = new double [size];
+
+    // for (int i = 0;i < tol;  ++i)
+    // {   
+    //     matrix[i] = new double[tol];
+    //     new_matrix[i] = new double[tol];        
+    // }
     //использую эту директиву мы определяем область кода, в которой матрица остается на GPU и разделяется между всеми ядрами в этой области 
     // enter data определяет начало времени жизни
     // create выделяет память на GPU но не копирует
-    #pragma acc enter data create(matrix[:tol][:tol],new_matrix[:tol][:tol])
+    #pragma acc enter data create(matrix[:size],new_matrix[:size],step_matrix[:size])
     //инициализируем
     init_array(matrix,tol);
     init_array(new_matrix,tol);
@@ -81,16 +100,16 @@ int main(int argc, char* argv[]){
 
         while(iter < iter_max)
         {
-            if(iter%tol==0){
+            // if(iter%tol==0){
                 
-            }
+            // }
             flag = !(iter%tol);
 
-            if(flag){
+            // if(flag){
                 // зануляем ошибкy на GPU
                 #pragma acc kernels
                 error = 0;
-            }
+            // }
             //тут мы параллелим , директива (loop) является директивой параллельного исполнения(Описываем какой тип параллелизма будем использовать)
             //с помощью условия collapse мы превращаем наши два вложенных цикла в один, тем самым выигрываем время
             // выполняем редукцию для error
@@ -102,28 +121,62 @@ int main(int argc, char* argv[]){
             {
                 for (int j = 1; j < tol-1; ++j)
                 {
-                    new_matrix[i][j] = 0.25 *( matrix[i][j+1] + matrix[i][j-1]+ matrix[i-1][j] + matrix[i+1][j]);
+                    new_matrix[IDX2C(i, j, tol)] = 0.25 *( matrix[IDX2C(i, j+1, tol)] + matrix[IDX2C(i, j-1, tol)]+ matrix[IDX2C(i-1,j, tol)] + matrix[IDX2C(i+1,j, tol)]);
                     // тут мы пересчитываем ошибку
-                    if(flag)
-                        error = fmax(error,std::abs(new_matrix[i][j] - matrix[i][j]));
+                    // if(flag)
+                    //     error = fmax(error,std::abs(new_matrix[i][j] - matrix[i][j]));
                 }
                 
             }
             // тут мы типа меняем матрицы местами 
-            double **step = matrix;
+            double *step = matrix;
             matrix = new_matrix;
             new_matrix = step;
 
             //проверяем ошибку 
 
-            if(flag)
-            {
+            // if(flag)
+            // {
+                #pragma acc data present(matrix, new_matrix, step_matrix) wait(1)
+                {
+                    #pragma acc host_data use_device(matrix, new_matrix, step_matrix)
+                    {
+                        //Эта функция копирует вектор new_matrix в вектор step_matrix.
+                        status = cublasDcopy(handle, size,new_matrix, step_inc, step_matrix, step_inc);
+                        if(status != CUBLAS_STATUS_SUCCESS) {
+                            std::cerr << "copy error" << std::endl; 
+                            exit(1);
+                        }
+                        //Эта функция умножает вектор  matrix на скаляр alpha
+                        //и добавляет его к вектору, step_matrix перезаписывая последний вектор с результатом.
+                        //умножаем вектор на скаляр и + вектор
+                        status = cublasDaxpy(handle, size, &alpha, matrix, step_inc, step_matrix, step_inc);
+                        if(status != CUBLAS_STATUS_SUCCESS) {
+                            std::cerr << "sum error" << std::endl;
+                            exit(1);
+                        }
+                        
+                        //Эта функция находит (наименьший) индекс элемента максимальной величины. 
+                        status = cublasIdamax(handle, size, step_matrix, step_inc, &max_ind);
+                        if(status != CUBLAS_STATUS_SUCCESS) {
+                            std::cerr << "abs max error" << std::endl; 
+                            exit(1);
+                        }
+                    
+                        // std::cout<<error<<std::endl;
+
+                        #pragma acc kernels present(error)
+                        error = fabs(step_matrix[max_ind - 1]);	
+                        
+                    }   
+                }
+
                 // тут мы обновляем нашу ошибочку на CPU значениями с GPU, ну и синхронизируем с помощью wait
                 #pragma acc update host(error) wait(1)
                 // тут если ошибка не превышает поданную ошибку err(которая наша точность), то мы заканчиваем цикл
                 if(error < err)
                     break;
-            }
+            // }
             
             iter++;
         }
@@ -140,18 +193,20 @@ int main(int argc, char* argv[]){
     //     {
     //         //используем  значения с GPU
     //         #pragma acc kernels present(matrix)
-    //         printf("%.2f\t", matrix[i][j]);
+    //         printf("%.2f\t", matrix[IDX2C(i, j, tol)]);
     //     }
     //     std::cout << std::endl;
     // }
     // std::cout << std::endl;
 
     // тут мы типа выходим из неструктурированной  секции дата и очищаем память
-    #pragma acc exit data delete(matrix[:tol][:tol], new_matrix[:tol][:tol])
-    for (int i = 0; i < tol; ++i){
-        delete[] matrix[i];
-        delete[] new_matrix[i];
-    }
+    #pragma acc exit data delete(matrix[:size], new_matrix[:size], step_matrix[:size])
+    // for (int i = 0; i < tol; ++i){
+    //     delete[] matrix[i];
+    //     delete[] new_matrix[i];
+    // }
+    cublasDestroy(handle);
+    delete[] step_matrix;
     delete[] matrix;
     delete[] new_matrix;
 
